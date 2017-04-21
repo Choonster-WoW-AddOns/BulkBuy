4957b5447965ab82077c8b1e355c8bf08e54d723
Choonster TheMage <Choonster.2010@gmail.com>
2017-04-21 21:51:43 +1000

    Update to account for 7.2 merchant frame changes
    -- If an item is sold for a non-gold currency, the AddOn will now buy the largest multiple of `stackCount` (the stack size sold by the merchant) less than `amount`(the amount entered in the stack split popup)

--- core.lua
+++ core.lua
@@ -17,19 +17,22 @@ local MAX_STACK = 10000 -- The maximum stack size accepted by the stack split fr
 
 local function BulkBuyMerchantItem(slot, amount)
 	local stackSize = GetMerchantItemMaxStack(slot)
-	local name, texture, price, quantity, numAvailable, isUsable, extendedCost = GetMerchantItemInfo(slot)
+	local name, texture, price, stackCount, numAvailable, isPurchasable, isUsable, extendedCost = GetMerchantItemInfo(slot)
 	
-	if price > 0 then -- Item is sold for gold, buy `amount` items
-		while amount > stackSize do -- Buy as many full stacks as we can
-			BuyMerchantItem(slot, stackSize)
-			amount = amount - stackSize
-		end
+	-- If the item is sold for a non-gold currency and can only be bought in stacks of `stackCount`, buy the largest multiple of `stackCount` less than `amount` possible.
+	if price <= 0 then 
+		amount = math.floor(amount / stackCount) * stackCount
+	end
+	
+	-- Otherwise the item is sold for gold, so buy `amount` items
+	
+	while amount > stackSize do -- Buy as many full stacks as we can
+		BuyMerchantItem(slot, stackSize)
+		amount = amount - stackSize
+	end
 		
-		if amount > 0 then -- Buy any leftover items
-			BuyMerchantItem(slot, amount)
-		end
-	else -- Item is sold for a non-gold currency and can only be bought in stacks of `quantity`, buy `amount * quantity` items
-		BuyMerchantItem(slot, amount * quantity)
+	if amount > 0 then -- Buy any leftover items
+		BuyMerchantItem(slot, amount)
 	end
 end
 
@@ -56,15 +59,19 @@ for i = 1, 10 do
 	button.SplitStack = MerchantItemButton_SplitStack
 end
 
-StaticPopupDialogs["CONFIRM_PURCHASE_TOKEN_ITEM"].OnAccept = function()
+local function ConfirmPopup_OnAccept()
 	BulkBuyMerchantItem(MerchantFrame.itemIndex, MerchantFrame.count or 1)
 end
+
+StaticPopupDialogs["CONFIRM_PURCHASE_TOKEN_ITEM"].OnAccept = ConfirmPopup_OnAccept
+StaticPopupDialogs["CONFIRM_PURCHASE_NONREFUNDABLE_ITEM"].OnAccept = ConfirmPopup_OnAccept
 
 local function MerchantItemButton_OnModifiedClick_Hook(self, button)
 	if self.hasStackSplit == 1 then
 		UpdateStackSplitFrame(MAX_STACK)
+		StackSplitFrame.minSplit = 1
 	elseif MerchantFrame.selectedTab == 1 and IsModifiedClick("SPLITSTACK") then
-		local _, _, _, stackCount, _, _, extendedCost = GetMerchantItemInfo(self:GetID())
+		local _, _, _, stackCount, _, _, _, extendedCost = GetMerchantItemInfo(self:GetID())
 		if stackCount > 1 and extendedCost then return end
 		
 		OpenStackSplitFrame(MAX_STACK, self, "BOTTOMLEFT", "TOPLEFT")
